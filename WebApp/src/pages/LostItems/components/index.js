0e4f1554bb8595ddd6a9e14e7634871453f238fa
kaleem-Durrani <kalimdurrani22@gmail.com>
2025-06-08 22:27:04 +0500

    feat: Export all LostItems components for improved modularity
    - Added exports for LostItemsStatistics, LostItemsFilters, LostItemsTable, LostItemFormModal, and ClaimItemModal to enhance component accessibility and organization within the LostItems module.

--- WebApp/src/pages/LostItems/components/index.js
+++ WebApp/src/pages/LostItems/components/index.js
@@ -1,1 +1,6 @@
-// Export components when created
+// Export all LostItems components
+export { default as LostItemsStatistics } from "./LostItemsStatistics.jsx";
+export { default as LostItemsFilters } from "./LostItemsFilters.jsx";
+export { default as LostItemsTable } from "./LostItemsTable.jsx";
+export { default as LostItemFormModal } from "./LostItemFormModal.jsx";
+export { default as ClaimItemModal } from "./ClaimItemModal.jsx";
