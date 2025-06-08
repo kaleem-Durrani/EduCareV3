49bf0536046196fab0fbb89715eb2f3a944709a5
kaleem-Durrani <kalimdurrani22@gmail.com>
2025-06-09 02:17:39 +0500

    feat: Wrap NavigationContainer with AuthProvider for authentication context
    - Integrated AuthProvider to manage user authentication state within the app.
    - Ensured that the navigation structure is now aware of the authentication context for better user experience.

--- EduCareMobile/App.tsx
+++ EduCareMobile/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
-// Import the root navigator
+// Import the root navigator and auth provider
 import { RootNavigator } from "./src/navigators";
+import { AuthProvider } from "./src/contexts";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
